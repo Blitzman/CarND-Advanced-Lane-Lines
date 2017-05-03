a04debe7e5fba85fb2571362a9c0f19e45e3d477
agarcia@dtic.ua.es <Albert Garca>
2017-05-03 20:45:26 +0200

    refactor for pipelining

--- all.py
+++ all.py
@@ -96,65 +96,62 @@ for test_image_filename in test_images_filenames:
     test_images.append(test_image)
 
 ###################################################################################################
-## Undistort Test Images
+## Pipeline
 ###################################################################################################
 
-undistorted_images = []
+def pipeline(img, filename):
 
-print()
-print("Undistorting...")
+    ###############################################################################################
+    ## Undistort Image
+    ###############################################################################################
 
-for test_image, test_image_filename in zip(test_images, test_images_filenames):
+    undistorted_image = None
 
-    print("Undistorting " + test_image_filename)
+    print()
+    print("Undistorting...")
 
-    undistorted_image = cv2.undistort(test_image, camera_mtx, dist_coeffs, None, camera_mtx)
-    undistorted_images.append(undistorted_image)
+    undistorted_image = cv2.undistort(img, camera_mtx, dist_coeffs, None, camera_mtx)
 
     # Plot original and undistorted image
     sbs.set_style("dark")
     f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
     #f.tight_layout()
-    ax1.imshow(test_image)
+    ax1.imshow(img)
     ax1.set_title('Original Image')
     ax2.imshow(undistorted_image)
     ax2.set_title('Undistorted Image')
-    f.savefig("test_undistorted/" + test_image_filename)
+    f.savefig("test_undistorted/" + filename)
 
-###################################################################################################
-## Thresholding
-###################################################################################################
+    ###################################################################################################
+    ## Thresholding
+    ###################################################################################################
 
-def threshold_x_gradient (img, sobel_size = 3, threshold = [0, 255]):
-    grayscale = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    def threshold_x_gradient (img, sobel_size = 3, threshold = [0, 255]):
+        grayscale = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     
-    sobel_x = cv2.Sobel(grayscale, cv2.CV_64F, 1, 0)
-    abs_sobel_x = np.absolute(sobel_x)
-    scaled_sobel_x = np.uint(255 * abs_sobel_x / np.max(abs_sobel_x))
+        sobel_x = cv2.Sobel(grayscale, cv2.CV_64F, 1, 0)
+        abs_sobel_x = np.absolute(sobel_x)
+        scaled_sobel_x = np.uint(255 * abs_sobel_x / np.max(abs_sobel_x))
 
-    binary_output = np.zeros_like(scaled_sobel_x)
-    binary_output[(scaled_sobel_x >= threshold[0]) & (scaled_sobel_x <= threshold[1])] = 1
+        binary_output = np.zeros_like(scaled_sobel_x)
+        binary_output[(scaled_sobel_x >= threshold[0]) & (scaled_sobel_x <= threshold[1])] = 1
 
-    return binary_output
+        return binary_output
 
-def threshold_hls_s_gradient (img, threshold = [0, 255]):
-    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
-    s_channel = hls[:, :, 2]
+    def threshold_hls_s_gradient (img, threshold = [0, 255]):
+        hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+        s_channel = hls[:, :, 2]
 
-    binary_output = np.zeros_like(s_channel)
-    binary_output[(s_channel >= threshold[0]) & (s_channel <= threshold[1])] = 1
+        binary_output = np.zeros_like(s_channel)
+        binary_output[(s_channel >= threshold[0]) & (s_channel <= threshold[1])] = 1
 
-    return binary_output
+        return binary_output
 
-thresholded_images = []
+    thresholded_image = None
 
-print()
-print("Thresholding...")
+    print()
+    print("Thresholding...")
 
-for undistorted_image, test_image_filename in zip(undistorted_images, test_images_filenames):
-
-    print("Thresholding " + test_image_filename)
-    
     x_binary = threshold_x_gradient(undistorted_image, 3, [20, 100])
     s_binary = threshold_hls_s_gradient(undistorted_image, [170, 255])
 
@@ -162,7 +159,6 @@ for undistorted_image, test_image_filename in zip(undistorted_images, test_image
 
     thresholded_image = np.zeros_like(x_binary)
     thresholded_image[(x_binary == 1) | (s_binary == 1)] = 1
-    thresholded_images.append(thresholded_image)
 
     sbs.set_style("dark")
     f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
@@ -170,56 +166,49 @@ for undistorted_image, test_image_filename in zip(undistorted_images, test_image
     ax1.imshow(undistorted_image)
     ax2.set_title('Stacked Thresholds')
     ax2.imshow(np.uint8(colored_binary * 255.999))
-    f.savefig("test_thresholded/" + test_image_filename)
+    f.savefig("test_thresholded/" + filename)
 
-###################################################################################################
-## Perspective Transform
-###################################################################################################
+    ###################################################################################################
+    ## Perspective Transform
+    ###################################################################################################
 
-print()
-print("Perspective transformations...")
+    print()
+    print("Perspective transformations...")
 
-def perspective_transform(img, src_points, dst_points):
+    def perspective_transform(img, src_points, dst_points):
 
-    img_size = (img.shape[1], img.shape[0])
-    src = np.float32(src_points)
-    dst = np.float32(dst_points)
+        img_size = (img.shape[1], img.shape[0])
+        src = np.float32(src_points)
+        dst = np.float32(dst_points)
 
-    M = cv2.getPerspectiveTransform(src, dst)
-    warped = cv2.warpPerspective(img, M, img_size)
+        M = cv2.getPerspectiveTransform(src, dst)
+        warped = cv2.warpPerspective(img, M, img_size)
 
-    return warped, M
+        return warped, M
 
-transformed_images = []
-transformed_matrices = []
+    transformed_image = None
+    transformation_matrix = None
 
-src_tl = [570, 470]
-src_tr = [720, 470]
-src_br = [1130, 720]
-src_bl = [200, 720]
+    src_tl = [570, 470]
+    src_tr = [720, 470]
+    src_br = [1130, 720]
+    src_bl = [200, 720]
 
-dst_tl = [320, 0]
-dst_tr = [980, 0]
-dst_br = [980, 720]
-dst_bl = [320, 720]
+    dst_tl = [320, 0]
+    dst_tr = [980, 0]
+    dst_br = [980, 720]
+    dst_bl = [320, 720]
 
-src_points = [src_tl, src_tr, src_br, src_bl]
-dst_points = [dst_tl, dst_tr, dst_br, dst_bl]
-
-for thresholded_image, test_image_filename in zip(thresholded_images, test_images_filenames):
-
-    print("Transforming " + test_image_filename)
+    src_points = [src_tl, src_tr, src_br, src_bl]
+    dst_points = [dst_tl, dst_tr, dst_br, dst_bl]
 
     thresholded_rgb = np.dstack((np.zeros_like(thresholded_image), thresholded_image, thresholded_image, thresholded_image))
     thresholded_rgb = (np.uint8(thresholded_rgb * 255.999))
 
-    transformed_image, transformed_m = perspective_transform(thresholded_rgb, src_points, dst_points)
+    transformed_image, transformation_matrix = perspective_transform(thresholded_rgb, src_points, dst_points)
 
     transformed_binary = np.zeros_like(thresholded_image)
     transformed_binary[(transformed_image[:, :, 0] > 0) | (transformed_image[:, :, 1] > 0) | (transformed_image[:, :, 2] > 0)] = 1 
-
-    transformed_images.append(transformed_binary)
-    transformed_matrices.append(transformed_m)
 
     sbs.set_style("dark")
     f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
@@ -227,99 +216,95 @@ for thresholded_image, test_image_filename in zip(thresholded_images, test_image
     ax1.imshow(thresholded_image)
     ax2.set_title('Transformed Image')
     ax2.imshow(np.uint8(transformed_binary * 255.999))
-    f.savefig("test_transformed/" + test_image_filename)
+    f.savefig("test_transformed/" + filename)
 
-###################################################################################################
-## Finding Lines
-###################################################################################################
+    ###################################################################################################
+    ## Finding Lines
+    ###################################################################################################
 
-print()
-print("Line finding...")
+    print()
+    print("Line finding...")
 
-def find_peaks (img, filename = None):
+    def find_peaks (img, filename = None):
     
-    histogram = np.sum(img[img.shape[0]//2:, :], axis = 0)
+        histogram = np.sum(img[img.shape[0]//2:, :], axis = 0)
 
-    m = np.int(histogram.shape[0]/2)
-    l = np.argmax(histogram[:m])
-    r = np.argmax(histogram[m:]) + m
+        m = np.int(histogram.shape[0]/2)
+        l = np.argmax(histogram[:m])
+        r = np.argmax(histogram[m:]) + m
 
-    if (filename != None):
-        sbs.set_style("dark")
-        f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
-        ax1.set_title('Original Image')
-        ax1.imshow(np.uint8(img * 255.999))
-        plt.plot(histogram)
-        ax2.set_title('Histogram')
-        ax2.imshow(np.uint8(img * 255.999))
-        f.savefig("test_histogram/" + filename)
+        if (filename != None):
+            sbs.set_style("dark")
+            f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
+            ax1.set_title('Original Image')
+            ax1.imshow(np.uint8(img * 255.999))
+            plt.plot(histogram)
+            ax2.set_title('Histogram')
+            ax2.imshow(np.uint8(img * 255.999))
+            f.savefig("test_histogram/" + filename)
 
-    return m, l, r
+        return m, l, r
 
-def sliding_window_lines (img, left, right, n_windows = 9, margin = 100, minpix = 100):
+    def sliding_window_lines (img, left, right, n_windows = 9, margin = 100, minpix = 100):
 
-    window_height = np.int(img.shape[0] / n_windows)
+        window_height = np.int(img.shape[0] / n_windows)
 
-    nonzero = img.nonzero()
-    nonzero_y = np.array(nonzero[0])
-    nonzero_x = np.array(nonzero[1])
+        nonzero = img.nonzero()
+        nonzero_y = np.array(nonzero[0])
+        nonzero_x = np.array(nonzero[1])
 
-    left_current = left
-    right_current = right
+        left_current = left
+        right_current = right
 
-    left_lane_indices = []
-    right_lane_indices = []
+        left_lane_indices = []
+        right_lane_indices = []
 
-    left_rects = []
-    right_rects = []
+        left_rects = []
+        right_rects = []
 
-    for window in range(n_windows):
+        for window in range(n_windows):
 
-        window_y_low = img.shape[0] - (window + 1) * window_height
-        window_y_high = img.shape[0] - window * window_height
-        window_x_left_low = left_current - margin
-        window_x_left_high = left_current + margin
-        window_x_right_low = right_current - margin
-        window_x_right_high = right_current + margin
+            window_y_low = img.shape[0] - (window + 1) * window_height
+            window_y_high = img.shape[0] - window * window_height
+            window_x_left_low = left_current - margin
+            window_x_left_high = left_current + margin
+            window_x_right_low = right_current - margin
+            window_x_right_high = right_current + margin
 
-        left_rects.append([(window_x_left_low, window_y_low), (window_x_left_high, window_y_high)])
-        right_rects.append([(window_x_right_low, window_y_low), (window_x_right_high, window_y_high)])
+            left_rects.append([(window_x_left_low, window_y_low), (window_x_left_high, window_y_high)])
+            right_rects.append([(window_x_right_low, window_y_low), (window_x_right_high, window_y_high)])
 
-        good_left_indices = ((nonzero_y >= window_y_low) & (nonzero_y < window_y_high) & (nonzero_x >= window_x_left_low) & (nonzero_x < window_x_left_high)).nonzero()[0]
-        good_right_indices = ((nonzero_y >= window_y_low) & (nonzero_y < window_y_high) & (nonzero_x >= window_x_right_low) & (nonzero_x < window_x_right_high)).nonzero()[0]
+            good_left_indices = ((nonzero_y >= window_y_low) & (nonzero_y < window_y_high) & (nonzero_x >= window_x_left_low) & (nonzero_x < window_x_left_high)).nonzero()[0]
+            good_right_indices = ((nonzero_y >= window_y_low) & (nonzero_y < window_y_high) & (nonzero_x >= window_x_right_low) & (nonzero_x < window_x_right_high)).nonzero()[0]
 
-        left_lane_indices.append(good_left_indices)
-        right_lane_indices.append(good_right_indices)
+            left_lane_indices.append(good_left_indices)
+            right_lane_indices.append(good_right_indices)
 
-        if len(good_left_indices) > minpix:
-            left_current = np.int(np.mean(nonzero_x[good_left_indices]))
-        if len(good_right_indices) > minpix:
-            right_current = np.int(np.mean(nonzero_x[good_right_indices]))
+            if len(good_left_indices) > minpix:
+                left_current = np.int(np.mean(nonzero_x[good_left_indices]))
+            if len(good_right_indices) > minpix:
+                right_current = np.int(np.mean(nonzero_x[good_right_indices]))
 
-    left_lane_indices = np.concatenate(left_lane_indices)
-    right_lane_indices = np.concatenate(right_lane_indices)
+        left_lane_indices = np.concatenate(left_lane_indices)
+        right_lane_indices = np.concatenate(right_lane_indices)
 
-    left_x = nonzero_x[left_lane_indices]
-    left_y = nonzero_y[left_lane_indices]
-    right_x = nonzero_x[right_lane_indices]
-    right_y = nonzero_y[right_lane_indices]
+        left_x = nonzero_x[left_lane_indices]
+        left_y = nonzero_y[left_lane_indices]
+        right_x = nonzero_x[right_lane_indices]
+        right_y = nonzero_y[right_lane_indices]
 
-    return left_rects, left_x, left_y, right_rects, right_x, right_y
+        return left_rects, left_x, left_y, right_rects, right_x, right_y
 
-lines_images = []
-left_lines = []
-right_lines = []
+    lines_image = None
+    left_line = None
+    right_line = None
 
-plot_y = np.linspace(0, transformed_image[0].shape[0]-1, transformed_image[0].shape[0])
+    plot_y = np.linspace(0, transformed_image[0].shape[0]-1, transformed_image[0].shape[0])
 
-for transformed_image, test_image_filename in zip(transformed_images, test_images_filenames):
+    mid, left, right = find_peaks(transformed_binary, filename)
+    left_r, left_x, left_y, right_r, right_x, right_y = sliding_window_lines(transformed_binary, left, right)
 
-    print("Finding lines " + test_image_filename)
-
-    mid, left, right = find_peaks(transformed_image, test_image_filename)
-    left_r, left_x, left_y, right_r, right_x, right_y = sliding_window_lines(transformed_image, left, right)
-
-    lines_image = (np.dstack((transformed_image, transformed_image, transformed_image)) * 255).astype(np.uint8).copy()
+    lines_image = (np.dstack((transformed_binary, transformed_binary, transformed_binary)) * 255).astype(np.uint8).copy()
 
     for left_rect, right_rect in zip(left_r, right_r):
         cv2.rectangle(lines_image, left_rect[0], left_rect[1], (0, 255, 0), 2)
@@ -334,8 +319,6 @@ for transformed_image, test_image_filename in zip(transformed_images, test_image
     lines_image[left_y, left_x] = [255, 0 ,0]
     lines_image[right_y, right_x] = [0, 0, 255]
 
-    lines_images.append(lines_image)
-
     left_line = Line()
     left_line.bestx = left_fit_x
     left_line.current_fit = left_fit
@@ -348,35 +331,28 @@ for transformed_image, test_image_filename in zip(transformed_images, test_image
     right_line.allx = right_x
     right_line.ally = right_y
 
-    left_lines.append(left_line)
-    right_lines.append(right_line)
-
     f = plt.figure()
     plt.imshow(lines_image)
     plt.plot(left_fit_x, plot_y, color='yellow')
     plt.plot(right_fit_x, plot_y, color='yellow')
     plt.xlim(0, 1280)
     plt.ylim(720, 0)
-    f.savefig("test_lines/" + test_image_filename)
+    f.savefig("test_lines/" + filename)
 
-###################################################################################################
-## Compute Curvature
-###################################################################################################
+    ###################################################################################################
+    ## Compute Curvature
+    ###################################################################################################
 
-print()
-print("Curvature computation...")
+    print()
+    print("Curvature computation...")
 
-def correct_curve(plot_y, line_x, curve_fit, ympp = 30/720, xmpp = 3.7/700):
-    curve_fit_cr = np.polyfit(plot_y * ympp, line_x * xmpp, 2)
-    return curve_fit_cr
+    def correct_curve(plot_y, line_x, curve_fit, ympp = 30/720, xmpp = 3.7/700):
+        curve_fit_cr = np.polyfit(plot_y * ympp, line_x * xmpp, 2)
+        return curve_fit_cr
 
-def compute_curvature(curve_fit, y_eval = 0, ympp = 30/720):
-    curvature = ((1 + (2 * curve_fit[0] * y_eval * ympp + curve_fit[1]) ** 2) ** 1.5) / np.absolute(2 * curve_fit[0])
-    return curvature
-
-for line_image, left_line, right_line, test_image_filename in zip(lines_images, left_lines, right_lines, test_images_filenames):
-
-    print("Computing curvature " + test_image_filename)
+    def compute_curvature(curve_fit, y_eval = 0, ympp = 30/720):
+        curvature = ((1 + (2 * curve_fit[0] * y_eval * ympp + curve_fit[1]) ** 2) ** 1.5) / np.absolute(2 * curve_fit[0])
+        return curvature
 
     left_curve_fit_corrected = correct_curve(plot_y, left_line.bestx, left_line.current_fit)
     left_line.curvature = compute_curvature(left_curve_fit_corrected, np.max(plot_y))
@@ -386,16 +362,14 @@ for line_image, left_line, right_line, test_image_filename in zip(lines_images, 
     print("Curvature left: " + str(left_line.curvature) + " meters")
     print("Curvature right: " + str(right_line.curvature) + " meters")
 
-###################################################################################################
-## Reprojection
-###################################################################################################
+    ###################################################################################################
+    ## Reprojection
+    ###################################################################################################
 
-print()
-print("Reprojection...")
+    print()
+    print("Reprojection...")
 
-for undistorted_image, transformed_image, transformed_matrix, left_line, right_line, test_image_filename in zip(undistorted_images, transformed_images, transformed_matrices, left_lines, right_lines, test_images_filenames):
-    
-    warp_zero = np.zeros_like(transformed_image).astype(np.uint8)
+    warp_zero = np.zeros_like(transformed_binary).astype(np.uint8)
     color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
 
     points_left = np.array([np.transpose(np.vstack([left_line.bestx, plot_y]))])
@@ -404,11 +378,14 @@ for undistorted_image, transformed_image, transformed_matrix, left_line, right_l
 
     cv2.fillPoly(color_warp, np.int_([points]), (0, 255, 0))
 
-    new_warp = cv2.warpPerspective(color_warp, np.linalg.inv(transformed_matrix), (transformed_image.shape[1], transformed_image.shape[0]))
+    new_warp = cv2.warpPerspective(color_warp, np.linalg.inv(transformation_matrix), (transformed_image.shape[1], transformed_image.shape[0]))
 
     result = cv2.addWeighted(undistorted_image, 1, new_warp, 0.3, 0)
 
     f = plt.figure()
     plt.imshow(result)
-    f.savefig("test_poly/" + test_image_filename)
+    f.savefig("test_poly/" + filename)
 
+for test_image, test_image_filename in zip(test_images, test_images_filenames):
+    print("Processing " + test_image_filename)
+    pipeline(test_image, test_image_filename)
